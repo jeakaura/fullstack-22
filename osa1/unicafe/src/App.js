07b6d73a0dcb089fb9bd3c749b63cec88343a29d
jeakaura <101410071+jeakaura@users.noreply.github.com>
2022-03-21 15:08:56 +0200

    unicafe step1

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const Display = ({ counter, text }) => (
+  <div>{text} {counter}</div>
+)
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>anna palautetta</h1>
+      <Button handleClick={() => setGood(good + 1)} text={"hyvä"} />
+      <Button handleClick={() => setNeutral(neutral + 1)} text={"neutraali"} />
+      <Button handleClick={() => setBad(bad + 1)} text={"huono"} />
+      <h1>statistiikat</h1>
+      <Display counter={good} text="hyvä" />
+      <Display counter={neutral} text="neutraali" />
+      <Display counter={bad} text="huono" />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
