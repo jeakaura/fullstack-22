5d49091c63a8253122e8fe81d56eb40734510d30
jeakaura <101410071+jeakaura@users.noreply.github.com>
2022-03-31 15:00:11 +0300

    puhelinluettelo step4

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,13 +2,21 @@ import { useState } from 'react'
 import Person from './components/Person'
 
 const App = () => {
-  const [persons, setPersons] = useState([]) 
-  const [newName, setNewName] = useState('')
+  const [persons, setPersons]         = useState([
+    { id: 1,  name: 'Arto Hellas',      number: '040-123456'    },
+    { id: 2,  name: 'Ada Lovelace',     number: '39-44-5323523' },
+    { id: 3,  name: 'Dan Abramov',      number: '12-43-234345'  },
+    { id: 4,  name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]) 
+  const [newName,     setNewName]     = useState('')
+  const [newNumber,   setNewNumber]   = useState('')
+  const [searchName,  setSearchName]  = useState('')
 
   const addName = (event) => {
     event.preventDefault()
     const noteObject = {
       name: newName,
+      number: newNumber,
       id: persons.length + 1,
     }
 
@@ -19,27 +27,50 @@ const App = () => {
       setPersons(persons.concat(noteObject))
     }
     setNewName('')
+    setNewNumber('')
   }
 
   const handleNoteChange = (event) => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilter = (event) => {
+    setSearchName(event.target.value)
+  };
+
+  const filtteri = persons.filter(person => person.name.toLowerCase().includes(searchName.toLowerCase()))
+
   return (
     <div>
-      <h2>Puhelinluettelo</h2>
+      <h1>Puhelinluettelo</h1>
+        <div>
+        hae nimellä:
+          <input onChange={handleFilter} />
+        </div>
+      <h2>Lisää uusi</h2>
       <form onSubmit={addName}>
         nimi:
         <input
           value={newName}
           onChange={handleNoteChange}
         />
+        <br />
+        numero:
+        <input
+          value={newNumber}
+          onChange={handleNumberChange}
+        />
+        <br />
         <button type="submit">lisää</button>
       </form>
       <h2>Numerot</h2>
       <div>
-        {persons.map(name => 
-          <Person key={name.id} person={name} />
+        {filtteri.map(name => 
+          <Person key={name.id} person={name} number={name.number} />
         )}
       </div>
     </div>
