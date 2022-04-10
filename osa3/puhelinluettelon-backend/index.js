1f1cc8bc87cecf56654a2d0183ffe782c3fbc448
jeakaura <101410071+jeakaura@users.noreply.github.com>
2022-04-10 22:53:56 +0300

    puhelinluettelon-backend step7

--- osa3/puhelinluettelon-backend/index.js
+++ osa3/puhelinluettelon-backend/index.js
@@ -1,5 +1,6 @@
 const { response } = require('express')
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 let persons = [
@@ -26,6 +27,7 @@ let persons = [
 ]
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 app.get('/', (req, res) => {
   res.send('<p>Hello world!</p>')
