d54f048dfedb1257ec497caee9364e095b65050c
jeakaura <101410071+jeakaura@users.noreply.github.com>
2022-04-11 16:15:40 +0300

    puhelinluettelo full stack (3.11)

--- osa3/puhelinluettelon-backend/index.js
+++ osa3/puhelinluettelon-backend/index.js
@@ -29,6 +29,7 @@ let persons = [
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :omatoken'))
 morgan.token('omatoken', (req, res) => {
