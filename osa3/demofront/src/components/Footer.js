1cae5cc781de97e393be3c23e670b3512bfee7aa
jeakaura <101410071+jeakaura@users.noreply.github.com>
2022-04-11 13:44:34 +0300

    osa3 demosovellus internetiin

---
+++ osa3/demofront/src/components/Footer.js
@@ -0,0 +1,16 @@
+const Footer = () => {
+    const footerStyle = {
+      color: 'green',
+      fontStyle: 'italic',
+      fontSize: 16
+    }
+  
+    return (
+      <div style={footerStyle}>
+        <br />
+        <em>Fullstack 2022 - Demosovellus - jeakaura</em>
+      </div>
+    )
+  }
+  
+  export default Footer
