3f9f3a26f23fc37e409cfc4fd97e6af7b3dd9756
jeakaura <101410071+jeakaura@users.noreply.github.com>
2022-05-13 15:05:14 +0300

    puhelinluettelo ja tietokanta, step3 & step4

--- osa3/puhelinluettelon-backend/index.js
+++ osa3/puhelinluettelon-backend/index.js
@@ -5,9 +5,11 @@ const cors = require('cors')
 require('dotenv').config()
 const Person = require('./models/person')
 
-app.use(express.json())
 app.use(cors())
+
 app.use(express.static('build'))
+
+app.use(express.json())
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :omatoken'))
 morgan.token('omatoken', (req, res) => {
@@ -20,32 +22,35 @@ app.get('/', (req, res) => {
 })
 
 app.get('/info', (req, res) => {
-    let LKM = persons.length
     let PVM = new Date()
-    res.send(`
-        <p>Puhelinluettelossa on ${LKM} henkilön tiedot</p>
+    Person.find({}).then((persons) => {
+      res.send(`
+        <p>Puhelinluettelossa on ${persons.length} henkilön tiedot</p>
         <p>Pyyntö tehtiin: ${PVM}</p>
-    `)
+      `)
+    })
   })
 
-app.get('/api/persons', (req, res) => {
+app.get('/api/persons', (req, res, next) => {
   Person.find({}).then(persons => {
     res.json(persons)
   })
+  .catch((error) => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-      response.json(person)
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
     })
+    .catch(error => next(error))
 })
 
-const generateId = () => {
-    const newId = Math.random() * (10000 - 4) + 4
-    return Math.floor(newId)
-}
-
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
   const body = req.body
 
   if (!body.name) {
@@ -64,20 +69,34 @@ app.post('/api/persons', (req, res) => {
   person.save().then(savedPerson => {
     res.json(savedPerson.toJSON())
   })
+  .catch((error) => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-  
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
+  response.status(404).send({ error: 'Tuntematon endpoint' })
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'Väärinmuotoiltu id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
