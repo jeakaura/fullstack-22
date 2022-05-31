6dcd4a4f4fb80ef4adba3fb9ee1117e3845123c3
jeakaura <101410071+jeakaura@users.noreply.github.com>
2022-05-31 20:37:15 +0300

    5.7 blogilistan frontend, step7

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,6 +1,19 @@
+import TogglableText from "./TogglableText"
+
 const Blog = ({blog}) => (
   <li className="blog">
     <b>{blog.title}</b> {blog.author}
+    <TogglableText buttonLabel="Avaa">
+      <small>
+      Aihe: <i> {blog.title} </i>
+      <br />
+      Kirjoittaja: <i> {blog.author} </i>
+      <br />
+      Tykkäykset: <i> {blog.likes} </i>
+      <br />
+      Osoite: <i> {blog.url} </i>
+      </small>
+    </TogglableText>
   </li>  
 )
 
