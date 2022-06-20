c0a9a468ff6cd41fc4874e2ffa928e2b5426663a
jeakaura <101410071+jeakaura@users.noreply.github.com>
2022-06-20 12:14:25 +0300

    5.18: blogilistan end to end -testit, step2

--- osa5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ osa5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,6 +1,12 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Matti Meikäläinen',
+      username: 'meikalainen',
+      password: 'salainen123'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user) 
     cy.visit('http://localhost:3000')
   })
 
@@ -11,4 +17,28 @@ describe('Blog app', function() {
     cy.get('#password')
     cy.contains('kirjaudu sisään')
   })
+
+  describe('Kirjautuminen',function() {
+    it('onnistuu oikeilla tiedoilla', function() {
+      cy.get('#username').type('meikalainen')
+      cy.get('#password').type('salainen123')
+      cy.get('#login-button').click()
+
+      cy.contains('Matti Meikäläinen on kirjautunut sisään')
+    })
+
+    it('epäonnistuu väärillä tiedoilla', function() {
+      cy.get('#username').type('meikalainen')
+      cy.get('#password').type('hakkeri2000')
+      cy.get('#login-button').click()
+
+      cy.get('.error')
+        .should('contain', 'Virhe: Käyttäjätunnus tai salasana ei kelpaa')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+        .and('have.css', 'border-style', 'solid')
+
+      cy.get('html').should('not.contain', 'Matti Meikäläinen on kirjautunut sisään')
+    })
+  })
+
 })
